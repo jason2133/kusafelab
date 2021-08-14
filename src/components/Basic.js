a436a19cf43b946d0d5d76ed7396691f219cda7d
unknown <jason2133@likelion.org>
2021-08-14 21:27:17 +0900

    toomyung

--- src/components/Basic.js
+++ src/components/Basic.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import './Bar.css'
+import './Basic.css'
 
 // <p>안전한 연구실을 구축하기 위한 노력!</p>
 // <p>고려대학교 스마트 캠퍼스 구축팀입니다.</p>
 
 const Basic = () => {
     return (
-        <>
-          <img src={require("../img/campus1.jpg").default} alt="" width="100%" height="100%" />
-          <p>안전한 연구실을 구축하기 위한 노력!</p>
-          <p>고려대학교 스마트 캠퍼스 구축팀입니다.</p>
-        </>
+        <div className="container">
+          <img src={require("../img/back70.PNG").default} alt="" width="100%" height="100%"/>
+          {/* <div className="topleft">
+          <h1>KU SAFE LAB</h1>
+          <h4>Smart Campus Connector,
+          <br/>Korea University</h4> 
+          </div> */}
+          <div className="content">
+            <p>안전한 연구실을 구축하기 위한 노력!</p>
+            <p>고려대학교 스마트캠퍼스 구축팀입니다.</p>
+          </div>
+        </div>
     )
 }
 
