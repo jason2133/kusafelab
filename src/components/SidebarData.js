32ece70b5ccb7ed6439edc3f99b0a81b07ec7a5f
unknown <jason2133@likelion.org>
2021-08-16 03:22:45 +0900

    font size edited

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,37 +1,29 @@
-import React from 'react'
-// import * as FaIcons from 'react-icons/fa'
-import * as AiIcons from 'react-icons/ai'
-import * as IoIcons from 'react-icons/io'
+// import React from 'react'
 
 export const SidebarData = [
     {
         title: "교내 연구실 안전사고 통계",
         path: '/',
-        icon: <AiIcons.AiFillHome />,
         cName: 'nav-text'
     },
     {
         title: '안전 수칙',
         path: '/',
-        icon: <IoIcons.IoIosPaper />,
         cName: 'nav-text'
     },
     {
         title: '비상시 대피 안내',
         path: '/',
-        icon: <AiIcons.AiFillHome />,
         cName: 'nav-text'
     },
     {
         title: '응급상황 전화번호',
         path: '/',
-        icon: <AiIcons.AiFillHome />,
         cName: 'nav-text'
     },
     {
         title: '비상시 대피 안내',
         path: '/',
-        icon: <AiIcons.AiFillHome />,
         cName: 'nav-text'
     },       
 ]
