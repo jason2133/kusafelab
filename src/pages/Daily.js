e6b1ef3dc0f1d5bccbb8f81997f09ce9d99eeb3a
unknown <jason2133@likelion.org>
2021-08-22 02:02:05 +0900

    go

--- src/pages/Daily.js
+++ src/pages/Daily.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Bar from '../components/Bar'
 import DetailDaily from './detail/DetailDaily'
 import Footer from '../components/Footer'
+// go
 
 function Daily() {
     return (
