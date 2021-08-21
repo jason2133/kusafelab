b1769f8e12fe80dfa1a7208611e5465930579502
unknown <jason2133@likelion.org>
2021-08-21 22:56:11 +0900

    fordeploy

--- src/ScrollToTop.js
+++ src/ScrollToTop.js
@@ -1,22 +1,3 @@
-// // import React, { Component } from 'react' 
-// import { Component } from 'react' 
-
-// import { withRouter } from 'react-router'
-
-// class ScrollToTop extends Component { 
-//     componentDidUpdate(prevProps) { 
-//         if (this.props.location !== prevProps.location) { 
-//             window.scrollTo(0, 0) 
-//         } 
-//     }
-    
-//     render() { 
-//         return this.props.children 
-//     } 
-// } 
-
-// export default withRouter(ScrollToTop)
-
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
